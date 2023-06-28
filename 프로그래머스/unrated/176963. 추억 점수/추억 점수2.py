dae04c51a68780c2a44caaa4ace82a89db6fb0ce
오유진 <102959835+0hyujin@users.noreply.github.com>
2023-06-28 12:30:52 +0900

    Create 추억 점수2.py

---
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수2.py
@@ -0,0 +1,10 @@
+def solution(name, yearning, photo):
+    answer = [] 
+    for people in photo: 
+        score = 0 
+        for person in people: 
+            if person in name: 
+                idx = name.index(person)
+                score += yearning[idx] 
+        answer.append(total_score) 
+    return answer 
