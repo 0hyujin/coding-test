548585e57e508cfe6a86790896355cf7e2ac916e
오유진 <102959835+0hyujin@users.noreply.github.com>
2023-06-28 12:28:51 +0900

    [unrated] Title: 추억 점수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.py
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.py
@@ -1,12 +1,5 @@
-def solution(name, yearning, photo):
-    answer = []
-    
-    for people in photo:
-        score = 0
-        for person in people:
-            
-            if person in name:
-                idx = name.index(person)
-                score += yearning[idx]
-        answer.append(score)
-    return answer
+def solution(이름, 점수, 사진):
+    return [sum(점수[이름.index(사람)] 
+                for 사람 in 사람들 
+                if 사람 in 이름) 
+            for 사람들 in 사진]
